08aa6c0a44617ec4aea2e4131d61b0eb7af5f119
LovroJukic <lovrojukic2010@gmail.com>
2026-01-02 18:31:02 +0100

    Start Google auth

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,4 +62,10 @@ dependencies {
     implementation("androidx.activity:activity-ktx:1.8.0")
     implementation("androidx.fragment:fragment-ktx:1.6.1")
     implementation("io.coil-kt:coil-compose:2.5.0")
+    implementation("com.google.firebase:firebase-auth-ktx:22.0.0")
+    implementation("com.google.android.gms:play-services-auth:20.6.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.1")
+    implementation("androidx.compose.runtime:runtime-livedata:1.4.3")
+    implementation("androidx.navigation:navigation-compose:2.6.0")
 }
